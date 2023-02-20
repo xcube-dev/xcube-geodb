fb732438a01f23dc5b675a141c481db9bad37024
Thomas <thomas.storm@brockmann-consult>
2023-02-20 23:16:33 +0100

    fixed https://github.com/dcs4cop/xcube-geodb/issues/58

--- tests/core/test_geodb.py
+++ tests/core/test_geodb.py
@@ -224,6 +224,10 @@ class GeoDBClientTest(unittest.TestCase):
                          str(bbox))
 
 
+        m.post(url, text='[{"geodb_estimate_collection_bbox":null}]')
+        bbox = self._api.get_collection_bbox('any')
+        self.assertIsNone(bbox)
+
     def test_rename_collection(self, m):
         self.set_global_mocks(m)
 
