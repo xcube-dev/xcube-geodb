ff8e69d899a5b9f6a466f41ba7811e5cb3657d6e
Thomas <thomas.storm@brockmann-consult>
2023-05-30 16:52:55 +0200

    using resources for testing finished

--- tests/core/test_stac.py
+++ tests/core/test_stac.py
@@ -12,7 +12,8 @@ class StacTest(unittest.TestCase):
             .files('tests.core.res.stac') \
             .joinpath('catalog.json')
         with resources.as_file(catalog_resource) as catalog_file:
-            collections = si.transform_stac_catalog(str(catalog_file.absolute()))
+            collections = si.transform_stac_catalog(
+                str(catalog_file.absolute()))
         self.assertEqual(collections[0].name, 'test_collection')
         self.assertEqual(22, len(collections[0].properties))
 
@@ -37,14 +38,22 @@ class StacTest(unittest.TestCase):
 
     def test_transformation_fails(self):
         si = StacTransformer()
-        with self.assertRaises(ValueError) as e:
-            si.transform_stac_catalog('./res/stac/catalog_2.json')
+        catalog_resource = resources \
+            .files('tests.core.res.stac') \
+            .joinpath('catalog_2.json')
+        with resources.as_file(catalog_resource) as catalog_file:
+            with self.assertRaises(ValueError) as e:
+                si.transform_stac_catalog(str(catalog_file.absolute()))
         self.assertEqual('Items with different CRS within a '
                          'collection not supported.', str(e.exception))
 
     def test_transformation_fails_no_crs(self):
         si = StacTransformer()
-        with self.assertRaises(ValueError) as e:
-            si.transform_stac_catalog('./res/stac/catalog_3.json')
+        catalog_resource = resources \
+            .files('tests.core.res.stac') \
+            .joinpath('catalog_3.json')
+        with resources.as_file(catalog_resource) as catalog_file:
+            with self.assertRaises(ValueError) as e:
+                si.transform_stac_catalog(str(catalog_file.absolute()))
         self.assertEqual('Collections without CRS not supported.',
                          str(e.exception))
