5a82de89f936c224fbae169cecdc3617631a9051
Thomas <thomas.storm@brockmann-consult.de>
2025-03-10 17:46:06 +0100

    schema cache automatically reloaded when collections are created, altered or dropped. Also, removed a trigger that did essentially nothing.

--- xcube_geodb/sql/geodb.sql
+++ xcube_geodb/sql/geodb.sql
@@ -129,20 +129,6 @@ CREATE TABLE IF NOT EXISTS public.geodb_user_databases
 GRANT SELECT, INSERT, UPDATE ON geodb_user_databases TO PUBLIC;
 GRANT SELECT, UPDATE, USAGE ON geodb_user_databases_seq TO PUBLIC;
 
-CREATE OR REPLACE FUNCTION public.notify_ddl_postgrest()
-    RETURNS event_trigger
-    LANGUAGE plpgsql
-AS
-$$
-BEGIN
-    NOTIFY ddl_command_end;
-END;
-$$;
-
-DROP EVENT TRIGGER IF EXISTS ddl_postgrest;
-CREATE EVENT TRIGGER ddl_postgrest ON ddl_command_end
-EXECUTE PROCEDURE public.notify_ddl_postgrest();
-
 -- ensures that database of collection belongs to user or group
 CREATE
     OR REPLACE FUNCTION public.geodb_user_allowed(
@@ -1685,3 +1671,60 @@ BEGIN
                 ) as res', collection);
 END
 $BODY$;
+
+-- Below: watching PostGREST schema cache changes to the database, and trigger a
+-- reload.
+-- Code copied from https://postgrest.org/en/stable/references/schema_cache.html.
+-- BEGIN of copied code
+-- watch CREATE and ALTER
+CREATE OR REPLACE FUNCTION pgrst_ddl_watch() RETURNS event_trigger AS
+$$
+DECLARE
+    cmd record;
+BEGIN
+    FOR cmd IN SELECT * FROM pg_event_trigger_ddl_commands()
+        LOOP
+            IF cmd.command_tag IN (
+                                   'CREATE SCHEMA', 'ALTER SCHEMA', 'CREATE TABLE', 'CREATE TABLE AS', 'SELECT INTO',
+                                   'ALTER TABLE', 'CREATE FOREIGN TABLE', 'ALTER FOREIGN TABLE', 'CREATE VIEW',
+                                   'ALTER VIEW', 'CREATE MATERIALIZED VIEW', 'ALTER MATERIALIZED VIEW',
+                                   'CREATE FUNCTION', 'ALTER FUNCTION', 'CREATE TRIGGER', 'CREATE TYPE', 'ALTER TYPE',
+                                   'CREATE RULE', 'COMMENT'
+                )
+                -- don't notify in case of CREATE TEMP table or other objects created on pg_temp
+                AND cmd.schema_name is distinct from 'pg_temp'
+            THEN
+                NOTIFY pgrst, 'reload schema';
+            END IF;
+        END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+-- watch DROP
+CREATE OR REPLACE FUNCTION pgrst_drop_watch() RETURNS event_trigger AS
+$$
+DECLARE
+    obj record;
+BEGIN
+    FOR obj IN SELECT * FROM pg_event_trigger_dropped_objects()
+        LOOP
+            IF obj.object_type IN (
+                                   'schema', 'table', 'foreign table', 'view', 'materialized view', 'function',
+                                   'trigger', 'type', 'rule'
+                )
+                AND obj.is_temporary IS false -- no pg_temp objects
+            THEN
+                NOTIFY pgrst, 'reload schema';
+            END IF;
+        END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE EVENT TRIGGER pgrst_ddl_watch
+    ON ddl_command_end
+EXECUTE PROCEDURE pgrst_ddl_watch();
+
+CREATE EVENT TRIGGER pgrst_drop_watch
+    ON sql_drop
+EXECUTE PROCEDURE pgrst_drop_watch();
+-- END of copied code
